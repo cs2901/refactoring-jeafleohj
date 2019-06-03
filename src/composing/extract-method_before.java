d46fe72e686e11d100d3db61eb5a82b9b9a1298f
Jhony Angulo <jhony.angulo@utec.edu.pe>
2019-06-03 10:43:56 -0500

    fix print

--- src/composing/extract-method_before.java
+++ src/composing/extract-method_before.java
@@ -1,7 +1,10 @@
+void printDetails(){
+    System.out.println("name: " + name);
+    System.out.println("amount: " + getOutstanding());
+}
 void printOwing() {
   printBanner();
-
+  printDetails()
   // Print details.
-  System.out.println("name: " + name);
-  System.out.println("amount: " + getOutstanding());
+
 }
